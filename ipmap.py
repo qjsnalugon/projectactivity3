5b063646fac72f11f89ec8cff4ca36609bfb8e71
qjsnalugon <qjsnalugon@tip.edu.ph>
2021-11-06 04:12:29 +0000

    adding files

---
+++ ipmap.py
@@ -0,0 +1,19 @@
+import ipapi 
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def Index():
+    search = request.form.get('search') 
+    data = ipapi.location(ip=search, output='json')
+    return render_template('index.html', data=data)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
